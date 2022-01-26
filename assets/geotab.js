7c9a2c742d208493a36baa437fbd059d9f0e0b10
amitrohan <amit@kruzr.co>
2022-01-26 19:37:00 +0530

    first commit

---
+++ assets/geotab.js
@@ -0,0 +1,94 @@
+/**
+* @returns {{initialize: Function, focus: Function, blur: Function}}
+*/
+geotab.addin.tripList = () => {
+  'use strict';
+
+  /* Scope variables */
+  let api; // Ref https://github.com/Geotab/mg-api-js
+  let state;
+  let angularAppInitCheckInterval;
+
+  /**
+   * Initialize the add-in
+   */
+  let initialize = () => {
+      console.log("Trip List Initialized");
+  };
+
+  /**
+   * Clears Angular Init check interval
+   */
+  let clearAngularAppinitCheck = () => {
+      clearInterval(angularAppInitCheckInterval);
+  };
+
+  let onAppStart = () => {
+      api.getSession((result) => {
+          angularAppInitCheckInterval = setInterval(() => {
+              if(window.myTripListNgAppRef && window.myTripListNgAppRef.zone){
+                  window.myTripListNgAppRef.zone.run(() => { window.myTripListNgAppRef.loadGeoTabSDKData(result.database,result.sessionId,result.database); });
+                  clearAngularAppinitCheck();
+              }else{
+                  console.log("Trip List app not ready yet, checking again");
+              }
+          },500)
+      });
+  };
+
+  /**
+  * Render
+  * App Logic
+  */
+  let render = () => {
+        console.log("Trip List Rendered");
+        onAppStart();
+  }
+
+  /**
+   * Aborts
+   */
+  let abort = () => {
+      console.log("Trip List Aborted");
+      clearAngularAppinitCheck();
+  };
+
+  return {
+      /*
+      * Page lifecycle method: initialize is called once when the Add-In first starts
+      * Use this function to initialize the Add-In's state such as default values or
+      * make API requests (Geotab or external) to ensure interface is ready for the user.
+      */
+      initialize(freshApi, freshState, callback) {
+
+          api = freshApi;
+          state = freshState;
+
+          initialize();
+          if (callback) {
+              callback();
+          }
+      },
+
+      /*
+      * Page lifecycle method: focus is called when the page has finished initialize method
+      * and again when a user leaves and returns to your Add-In that has already been initialized.
+      * Use this function to refresh state such as vehicles, zones or exceptions which may have
+      * been modified since it was first initialized.
+      */
+      focus(freshApi, freshState) {
+          api = freshApi;
+          state = freshState;
+
+          render();
+      },
+
+      /*
+      * Page lifecycle method: blur is called when the user is leaving your Add-In.
+      * Use this function to save state or commit changes to a datastore or release memory.
+      */
+      blur() {
+          abort();
+      }
+  };
+};
